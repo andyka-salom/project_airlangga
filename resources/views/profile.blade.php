4f14c1cba319650b62628cf6f2f497fe7436171e
faerosjahera <ifairuzahira@gmail.com>
2023-12-22 19:09:53 +0700

    benerin footer

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,14 +1,18 @@
-
 @extends('layouts.app')
 
 @section('content')
     <div class="container">
-        <h3>Profil Pengguna</h3>
-
+        <h1>Profil Pengguna</h1>
+        <div class="outer-container">
         <div id="profil-container">
             {{-- Foto Profil --}}
             <div id="foto-profil-container">
-
+                @if($user->photo)
+                    <img id="foto-profil" src="{{ asset('fotouser/'. $user->photo) }}" alt="Foto Profil" class="clickable-photo">
+                @else
+                    <img id="foto-profil" src="{{ asset('avatar.png') }}" alt="Default Foto Profil" class="clickable-photo">
+                @endif
+                <br>
             </div>
 
             {{-- Informasi Profil --}}
@@ -16,7 +20,10 @@
                 <form id="edit-profil-form" method="post" action="{{ route('profil.update') }}" enctype="multipart/form-data">
                     @csrf
                     @method('put')
-
+                    <div class="center-label">
+                    <label for="edit-foto-profil" class="btn btn-primary">Ganti Foto Profil</label>
+                    </div>
+                    <input type="file" name="photo" id="edit-foto-profil" class="form-control" style="display: none;">
                     <label for="edit-nama">Nama:</label>
                     <input type="text" id="edit-nama" name="name" value="{{ $user->name }}">
 
@@ -28,7 +35,7 @@
 
                     <label for="edit-password">Password Baru:</label>
                     <input type="password" id="edit-password" name="password">
-
+                    <button type="submit" class="btn btn-primary">Simpan</button>
                 </form>
             </div>
         </div>
@@ -38,12 +45,28 @@
             @foreach($serviceProviders as $provider)
                 <div class="card">
                     <img src="{{ asset('penyediaImages/' . $provider->photo) }}" alt="Provider Photo">
-
+                    <h3>NAMA  : {{ $provider->nama_toko }}</h3>
                     <p>Alamat: {{ $provider->address }}</p>
                     <p>Deskripsi: {{ $provider->description }}</p>
                     <p>Status: {{ $provider->status }}</p>
                     <p>Harga: {{ $provider->Harga }}</p>
-
+                    <h4>Latest Reviews:</h4>         
+                    <div class="reviews-container">
+                        @foreach($provider->reviews->take(3) as $review)
+                            <div class="review-card">
+                                <img class="review-user-photo" src="{{ asset('fotouser/' . $review->user->photo) }}" alt="{{ $review->user->name }}'s Photo">
+                                <p class="review-username">{{ $review->user->name }}</p>
+                                <p class="review-rating">{{ $review->rating }}</p>
+                                <p class="review-comment">{{ $review->comment }}</p>
+                                <!-- Display other review details as needed -->
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            @endforeach
+        @endif
+        </div>
+    </div>
 
     <!-- Modal for displaying larger profile picture -->
     <div class="modal fade" id="photoModal" tabindex="-1" aria-labelledby="photoModalLabel" aria-hidden="true">
