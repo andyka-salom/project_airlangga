8cce226b66ed97e2e801eaf2a90736de5cf6d0ff
faerosjahera <ifairuzahira@gmail.com>
2023-12-26 16:04:21 +0700

    cek n update

--- resources/views/cust/categoryshow-penyedia.blade.php
+++ resources/views/cust/categoryshow-penyedia.blade.php
@@ -2,58 +2,52 @@
 
 @section('content')
     <section class="section-padding">
-        <div class ="containerpenyedia">
         <div class="container">
             <div class="row">
-    
                 <div class="col-lg-12 col-12 text-center">
                     <h1 class="mb-4">Profil Penyedia Jasa</h1>
                 </div>
-    
+
                 @forelse ($profilPenyediaJasas as $profilPenyediaJasa)
-                <div class="col-lg-8 col-12 mt-3 mx-auto">
-                    <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
-                        <div class="d-flex">
-                            @if($profilPenyediaJasa->photo)
-                            <img src="{{ asset('penyediaImages/' . $profilPenyediaJasa->photo) }}" class="img-fluid mb-3" width="100%" height="300px" alt="">
-                            @else
-                            Tidak ada foto
-                            @endif
-                            <div class="custom-block-topics-listing-info d-flex">
-                                <div>
+                    <div class="col-lg-8 col-12 mt-3 mx-auto">
+                        <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
+                            <div class="d-flex">
+                                @if($profilPenyediaJasa->photo)
+                                    <img src="{{ asset('penyediaImages/' . $profilPenyediaJasa->photo) }}" class="img-fluid mb-3" width="300px" height="100%" alt="{{ $profilPenyediaJasa->nama_toko }}">
+                                @else
+                                    <div class="no-photo-placeholder">
+                                        Tidak ada foto
+                                    </div>
+                                @endif
+
+                                <div class="custom-block-topics-listing-info d-flex flex-column ml-3">
                                     <h5 class="mb-2">{{ $profilPenyediaJasa->nama_toko }}</h5>
                                     <p class="mb-0"><strong>Jasa:</strong> {{ $profilPenyediaJasa->jasa->nama_jasa }}</p>
                                     <p class="mb-0"><strong>Alamat:</strong> {{ $profilPenyediaJasa->address }}</p>
-    
                                     <p class="mb-0">{{ $profilPenyediaJasa->description }}</p>
-    
                                     <p class="btn custom-btn mt-3 mt-lg-4">{{ $profilPenyediaJasa->Harga ?? 'Tidak ada data' }}</p>
-                                    
-                                     
-                                <div>
-                                    <a href="{{ route('order', $profilPenyediaJasa->id_provider) }}" class="btn btn-success">
-                                    <iconify-icon icon="icon-park-outline:shopping-bag"></iconify-icon> Order
-                                </a>
-                                <a href="{{ route('chatify.room', ['userId' => $profilPenyediaJasa->id_user]) }}" class="btn btn-success">
-                                    <iconify-icon icon="ep:chat-dot-round"></iconify-icon> Chat
-                                </a>
-                                <a href="{{ route('resume', ['user' => $profilPenyediaJasa->id_user, 'providerId' => $profilPenyediaJasa->id_provider]) }}"  class="btn btn-success">
-                                    <iconify-icon icon="mdi:resume"></iconify-icon> Resume
-                                </a>
+
+                                    <div class="d-flex mt-auto">
+                                        <a href="{{ route('order', $profilPenyediaJasa->id_provider) }}" class="btn btn-success mr-2" style="margin-right: 4px">
+                                            <iconify-icon icon="icon-park-outline:shopping-bag"></iconify-icon> Order
+                                        </a>
+                                        <a href="{{ route('chatify.room', ['userId' => $profilPenyediaJasa->id_user]) }}" class="btn btn-success mr-2" style="margin-right: 4px">
+                                            <iconify-icon icon="ep:chat-dot-round"></iconify-icon> Chat
+                                        </a>
+                                        <a href="{{ route('resume', ['user' => $profilPenyediaJasa->id_user, 'providerId' => $profilPenyediaJasa->id_provider]) }}" class="btn btn-success" style="margin-right: 4px">
+                                            <iconify-icon icon="mdi:resume"></iconify-icon> Resume
+                                        </a>
+                                    </div>
                                 </div>
-                                    
-                                </div>
-    
-                                
                             </div>
                         </div>
                     </div>
-                    @empty
-                    <p>Tidak ada profil penyedia jasa yang ditemukan.</p>
-                    @endforelse
-                </div>
-    </section>    
-
-
+                @empty
+                    <div class="col-lg-8 col-12 mt-3 mx-auto">
+                        <p class="text-muted">Tidak ada profil penyedia jasa yang ditemukan.</p>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </section>
 @endsection
-
